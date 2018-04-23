7fb85d80cd0bc7e974faf0b2898e74180ce5d559
LeoAbalan <38656353+LeoAbalan@users.noreply.github.com>
2018-04-24 01:03:20 +0200

    Update test_2.py

--- code/python/test_2.py
+++ code/python/test_2.py
@@ -1,19 +1,17 @@
 from __future__ import division
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-from fonction import *
-from ligne import *
+from fonction import * #On importe le fichier "fonction" où se trouve les fonctions stop et feu
+from ligne import *    #On importe le fichier "ligne" où se trouve la fonction ligne2
 import RPi.GPIO as gpio
 import sys
 import smbus
 import time
 import cv2
-import numpy as np
+import numpy as np#On importe toutes les librairies nécéssaires 
 
 
-adress=0x03
-
-#adresse I2C
+adress=0x03#adresse I2C
 bus=smbus.SMBus(1)
 
 time.sleep(2)
@@ -27,18 +25,18 @@ flagG =0
 flagDG=0
 flagA =0
 flagF =0
-flagAV =0
-camera=PiCamera()
-camera.resolution=(1250, 600)#L*H
-camera.framerate=49
-rawCapture = PiRGBArray(camera, size=(1250,600))
+flagAV =0#déclaration des flags à 0
+camera=PiCamera()#déclaration de la caméra
+camera.resolution=(1250, 600)#Longueur*Hauteur
+#On définit ka résolution de l'image récupérée par la caméra
+camera.framerate=49#On définit le nombre d'image par seconde récupéré par la caméra
+rawCapture = PiRGBArray(camera, size=(1250,600))#déclaration  d'un array d'images dont le format correspond à la résolution attribué à la caméra
 
 time.sleep(0.1)
 #CVPoint objectPos = cvPoint(-1,-1)
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):#On demande à la caméra de capturer un stream d'array d'images (vidéo) en temps réel
 
-
-    image = frame.array
+    image = frame.array #On déclare une frame correspondant stream vidéo capturé par la caméra
     #cv2.imshow("Frame", image)
 
     key =cv2.waitKey(1) & 0xFF
@@ -47,9 +45,8 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     #Im=cv2.imread(image[1])
     #hsv=cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    im_gray=cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    hsv2 = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-
+    im_gray=cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) #On définit une frame correspondant stream vidéo capturé par la caméra convertit en gray
+    hsv2 = cv2.cvtColor(image, cv2.COLOR_RGB2HSV) #On définit une frame correspondant stream vidéo capturé par la caméra convertit en hsv
     (thresh, im_bw) =cv2.threshold(im_gray, 128,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
 
@@ -63,8 +60,8 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 
 
-    cropped = image [500:600 , 1:1250]
-    hsv = cv2.cvtColor(cropped, cv2.COLOR_RGB2HSV)
+    cropped = image [500:600 , 1:1250]#déclaration de la frame "cropped" correspondant à une frame ne reprenant qu'une section de la partie inférieure de la frame "image"
+    hsv = cv2.cvtColor(cropped, cv2.COLOR_RGB2HSV)#On définit une frame correspondant à la frame "cropped" convertit en hsv
 
 
 
@@ -74,6 +71,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     upper_white=np.array([131, 255, 255])
 
     mask=cv2.inRange(hsv, lower_white, upper_white)
+    #On définit l'intervale des couleurs en RGB que l'on veut détecter pour le suivit de ligne blanche
 
     test = cv2.bitwise_and(cropped, cropped, mask=mask)
     
@@ -86,7 +84,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     contours,hierarchy = cv2.findContours(dilated_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours,key=cv2.contourArea, reverse=True)[:1]
-
+    
+    #Détection de la plus grande zone comprise dans l'intervale des couleurs sur la frame cropped
+    #On utilise la frame "cropped" pour améliorer la précision du suivit de ligne
     
 
 
@@ -95,11 +95,11 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     """ ligne blanche 
     """
     if flagAV==0 :
-        bus.write_byte(adress,1)
+        bus.write_byte(adress,1) #On commande à la voiture de se mettre en marche
         print("marche")
         time.sleep(0.01)
-        flagAV=1
-
+        flagAV=1 #Le flag se met à 1 pour que la commmande de mise marche ne soit appelée qu'une fois, et donc améliore les performances du système
+  
     
     
                   
@@ -109,13 +109,16 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     #if flagF==0 :
 
-    cx2=ligne2(cropped, contours)
+    cx2=ligne2(cropped, contours)#On appelle la fonction ligne2 du fichier "ligne" qui permet de détecter le barycentre de la plus grosse zone de couleur blanche sur la frame "cropped" et d'assurer le suivit de ligne
+    #La coordonnée du baycentre en abscisse est récupéré en tant que "cx2"
+    
+    
     print("cx2", cx2)
     #flagDG,flagD, flagDD, flagG, flagA=ligne (cropped, flagD, flagDD, flagG, flagA, flagDG, contours)
     #    flagF=1
 
     #if flagF==0 :
-    flag=stop(im_gray, flag, image)
+    flag=stop(im_gray, flag, image)#On appelle la fonction stop du fichier "fonction" qui permet la détection du panneau stop, et l'arrêt des moteurs à sa détection
      #  flagF=1
     """
 
