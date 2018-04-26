8df900b1ed7587144895c64a1342998b8178d7fb
LeoAbalan <38656353+LeoAbalan@users.noreply.github.com>
2018-04-26 16:05:56 +0200

    Add files via upload

---
+++ code/python final/fonction.py
@@ -0,0 +1,143 @@
+from __future__ import division
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import smbus
+import time
+import cv2
+import numpy as np
+
+#adresse I2C
+bus=smbus.SMBus(1)
+adress=0x1a
+
+
+
+
+
+""" premiere methode detection du feux """
+
+
+
+def feu(im_gray,flag, image):                  
+
+    #detection feu rouge
+
+    templateFeu = cv2.imread('feu_rouge.png',0)
+
+    
+
+    
+    wF, hF = templateFeu.shape[::-1]
+    resFeu =cv2.matchTemplate(im_gray, templateFeu ,cv2.TM_CCOEFF_NORMED)
+    thresholdFeu = 0.8
+    locFeu= np.where( resFeu >= thresholdFeu)
+    if np.any( resFeu >= thresholdFeu):
+        print("feu rouge")
+        bus.write_byte(adress,6)
+        time.sleep(5)
+        flag =0
+        print("flag : ", flag)
+        if flag ==0 :
+            bus.write_byte(adress,4)
+            print("avancer")
+            time.sleep(1)
+            flag =1
+    else :
+        if flag ==0:
+            print("avancer")
+            bus.write_byte(adress,4)
+            flag =1
+    for pt in zip(*locFeu[::-1]):
+                cv2.rectangle(image, pt, (pt[0]+wF,pt[1]+hF),(0,0,255),2)
+
+
+    return flag
+
+
+
+
+
+
+
+
+""" premiere methode detection du stop """
+
+
+def stop(im_gray,flag, image):
+
+    #detection stop
+
+    template = cv2.imread('stop.png',0)
+    
+    
+    w, h = template.shape[::-1]
+    res =cv2.matchTemplate(im_gray, template ,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.3
+    loc= np.where( res >= threshold)
+    if np.any( res >= threshold):
+        print("stop")
+        bus.write_byte(adress,2)
+        time.sleep(5)
+        flag =0
+        
+        if flag ==0:
+            bus.write_byte(adress,1)
+            time.sleep(0.1)
+            print("avancer")
+            
+            flag =1
+    else :
+        
+        if flag ==0:
+            print("avancer_stop")
+            bus.write_byte(adress,1)
+            time.sleep(0.5)
+            flag =1
+    for pt in zip(*loc[::-1]):
+                  cv2.rectangle(image, pt, (pt[0]+w,pt[1]+h),(0,0,255),2)
+
+    return flag
+
+
+
+
+
+
+""" deuxieme methode detection du stop """
+
+
+
+
+
+def stop2(hsv2,flag, image):
+
+    lower_red=np.array([115, 160, 120])
+    upper_red=np.array([140, 255, 200])
+    mask2=cv2.inRange(hsv2, lower_red,upper_red )
+
+    kernel_erode2 = np.ones((4,4), np.uint8)
+    eroded_mask2 =cv2.erode(mask2,kernel_erode2,iterations=1)
+    kernel_dilate2 = np.ones((6,6), np.uint8)
+    dilated_mask2 =cv2.dilate(eroded_mask2, kernel_dilate2, iterations=1)
+
+    contours2,hierarchy = cv2.findContours(dilated_mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours2 = sorted(contours2,key=cv2.contourArea, reverse=True)[:1]
+
+    if len(contours2) > 0:
+         M2=cv2.moments(contours2[0])
+
+         cx2=int(M2['m10']/M2['m00'])
+         cy2= int(M2['m01']/M2['m00'])
+
+
+         cv2.circle(hsv2,(cx2,cy2),15,(255,0, 0),5)
+         print"stop"
+         bus.write_byte(adress,2)
+         time.sleep(0.1)
+         bus.write_byte(adress,1)
+         time.sleep(0.1)
+         flag=1
+
+    
+
+    return flag
---
+++ code/python final/main.py
@@ -0,0 +1,115 @@
+from __future__ import division
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from fonction import *
+from ligne import *
+import RPi.GPIO as gpio
+import sys
+import smbus
+import time
+import cv2
+import numpy as np
+
+
+adress=0x03
+
+#adresse I2C
+bus=smbus.SMBus(1)
+
+time.sleep(2)
+print("adresse I2C", adress)
+gpio.setmode(gpio.BCM)
+gpio.setup(17, gpio.OUT)
+flag =0
+flagAV =0
+camera=PiCamera()
+camera.resolution=(1250, 600)#L*H
+camera.framerate=49
+rawCapture = PiRGBArray(camera, size=(1250,600))
+
+
+
+time.sleep(0.1)
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+
+
+
+    """ traitement d'images """
+    
+    image = frame.array
+
+    key =cv2.waitKey(1) & 0xFF
+    
+    rawCapture.truncate(0)
+
+    im_gray=cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    hsv2 = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+
+    (thresh, im_bw) =cv2.threshold(im_gray, 128,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    
+    hsv2 = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    cropped = image [500:600 , 1:1250]
+    invGamma = 10
+    table = np.array([((i / 255.0) ** invGamma) * 255
+	for i in np.arange(0, 256)]).astype("uint8")
+    test2 =cv2.LUT(cropped, table)
+    hsv = cv2.cvtColor(test2, cv2.COLOR_RGB2HSV)
+    ret, seuil =cv2.threshold (hsv, 0,255,0)
+    lower_white=np.array([0, 0, 150])
+    upper_white=np.array([230, 230, 255])
+    mask=cv2.inRange(hsv, lower_white, upper_white)
+    test = cv2.bitwise_and(cropped, cropped, mask=mask)
+    kernel_erode = np.ones((4,4), np.uint8)
+    eroded_mask =cv2.erode(mask,kernel_erode,iterations=1)
+    kernel_dilate = np.ones((6,6), np.uint8)
+    dilated_mask =cv2.dilate(eroded_mask, kernel_dilate, iterations=1)
+    contours,hierarchy = cv2.findContours(dilated_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours,key=cv2.contourArea, reverse=True)[:1]
+
+
+
+    """ demarrage moteur """
+
+    if flagAV==0 :
+        bus.write_byte(adress,1)
+        print("marche")
+        time.sleep(0.01)
+        flagAV=1
+
+
+
+    """ ligne blanche """
+        
+    cx2=ligne2(cropped, contours)
+    print("cx2", cx2)
+
+
+
+
+    """ fonction pour le stop """""
+    
+    if flag==0 :
+        flag=stop2(hsv2, flag, image)
+        if flag==1 :
+         bus.write_byte(adress,2)
+         time.sleep(3)
+         bus.write_byte(adress,1)
+        time.sleep(0.1)
+    
+
+
+
+
+
+
+
+    """ affichage des frames
+
+
+
+    cv2.imshow("Frame",image)
+    cv2.imshow("cropped", cropped)
+    cv2.imshow("Fram2e",hsv2)
+    """
+    
