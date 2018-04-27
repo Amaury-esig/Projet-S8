f59d4b15b98c28fe7213cc757a53009c7ecd5f4b
amaury reille <amaury.reille@groupe-esigelec.org>
2018-04-27 12:57:08 +0200

    arduino
    arduino

--- code/arduino/code_final_arduino_projet.ino
+++ code/arduino/code_final_arduino_projet.ino
@@ -2,7 +2,7 @@
 #include <Servo.h> 
  #include <Wire.h>
 
-#define SLAVE_ADDRESS 0x03
+#define SLAVE_ADDRESS 0x1a
 int dataReceived = 0;
 int commande;
 int flag=0;
@@ -10,19 +10,17 @@ int temps = 1500; //censé être à mi-chemin entre 1000 et 2000, un bon point d
 int vitesse = 1500;
 Servo monServo;
 Servo moteur;
-/*
+
  const char DOUT_TRIGGER_1 = 7;
 const char DIN_ECHO_1 = 6;
  const char DOUT_TRIGGER_2 = 4;
 const char DIN_ECHO_2 = 5;
-
-
  const char DOUT_TRIGGER_3 = 13;
 const char DIN_ECHO_3 = 10;
 float distance_1;
 float distance_3;
 float distance_2;
-*/
+
 
 void setup()
 {
@@ -31,12 +29,12 @@ void setup()
     Wire.onReceive(receiveData);
    // Wire.onRequest(sendData);
     //Serial.println("Hello World");
-  /*  pinMode(DOUT_TRIGGER_1, OUTPUT);
+    pinMode(DOUT_TRIGGER_1, OUTPUT);
     pinMode(DIN_ECHO_1, INPUT);
     pinMode(DOUT_TRIGGER_2, OUTPUT);
     pinMode(DIN_ECHO_2, INPUT);
     pinMode(DOUT_TRIGGER_3, OUTPUT);
-    pinMode(DIN_ECHO_3, INPUT);*/
+    pinMode(DIN_ECHO_3, INPUT);
     monServo.attach(11);
     moteur.attach(9);
     //on démarre à une valeur censé être la moitié de
@@ -58,7 +56,7 @@ void loop()
 
 ////*********************foncton pour la mesure de distance
 
-/*
+
 float dist_1(){
  digitalWrite(DOUT_TRIGGER_1, LOW);
     delayMicroseconds(2);
@@ -92,7 +90,7 @@ float dist_2(){
   }
 
 
-*/
+
 
 
 
@@ -112,44 +110,69 @@ flag =1;
         //on modifie la consigne si c'est un caractère qui nous intéresse
         if(commande == 1){
           
-        vitesse = 1586;
-        //vitesse = 100;
-        }
-       else if(commande == 2){
+        moteur.writeMicroseconds(1587);
+          delay(100);
+            temps = 2000;}
+        if(commande == 2){
           
-        vitesse = 1500;
-        //vitesse = 90;
-        }
-       else{
-
-          if(commande < 79 || commande > 102){
-            vitesse = 1587;
-            //vitesse = 110;
-          }
-          else {
-          vitesse = 1585;
-          //vitesse = 100;
-          }
-        temps = commande;
-        
+        moteur.writeMicroseconds(1587);
+            temps = 1000;} 
+        if(commande ==3){
+                  moteur.writeMicroseconds(1500);
+         monServo.write(1500);
+         delay(1000);
+        moteur.writeMicroseconds(1586);
+       // temps =1500;
        }
 
+        if(commande ==7){
+          
+        moteur.writeMicroseconds(1500);
+         monServo.write(1750);
+         delay(1000);
+        moteur.writeMicroseconds(1589);
+        //temps =1750;
+        }
 
-
-
-
-
-
-
-        monServo.write(temps);
-        //if( (dist_1()>50.0) and (dist_2()>25.0) and (dist_3()>25.0)   ){
-        moteur.writeMicroseconds(vitesse);
-        //}
-        //else{
+        if(commande ==8){
+         moteur.writeMicroseconds(1500);
+         monServo.write(1250);
+         delay(1000);  
         
-        //moteur.writeMicroseconds(1500);
+        moteur.writeMicroseconds(1589);
+        //temps =1250;
+        }
+
+
+
+
+
+
+
+        
+        if (commande ==4)
+        vitesse= 1586;
+
+        if (commande ==5)
+        vitesse =1350;
+
+        else if (commande ==6)
+        vitesse = 1500;
+
+
+        
+
+ monServo.write(temps);
+
+
+        if( (dist_1()>10.0) and (dist_2()>5.0) and (dist_3()>5.0)   ){
+        moteur.writeMicroseconds(vitesse);
+        }
+        else{
+        
+        moteur.writeMicroseconds(1500);
      //   Serial.println("\nobstacle present, arret du moteur");
-        //}
+        }
 
     
 }
